18083692d935716ba5adb1ec036069f2c18835ba
Shah Sahil <shahsahil713@gmail.com>
2020-06-10 13:54:31 +0530

    Updates

---
+++ precache-manifest.4a2d86d047de31c2d43b94ba3ce7f0fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b29415815ada3780590debedef70ce3",
+    "url": "/React_ToDoApp/index.html"
+  },
+  {
+    "revision": "2d27a77011137f731898",
+    "url": "/React_ToDoApp/static/css/main.039a1d9f.chunk.css"
+  },
+  {
+    "revision": "fc6deaa18904a427f7f9",
+    "url": "/React_ToDoApp/static/js/2.182eb09d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_ToDoApp/static/js/2.182eb09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d27a77011137f731898",
+    "url": "/React_ToDoApp/static/js/main.9516a336.chunk.js"
+  },
+  {
+    "revision": "bfad5cddac1423fcd0f8",
+    "url": "/React_ToDoApp/static/js/runtime-main.6b0559bf.js"
+  }
+]);
